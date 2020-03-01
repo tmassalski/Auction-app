734ceb4528f40e082bb977d5536615ed5142acf0
Tomasz Massalski <tomasz.massalski@gmail.com>
2020-03-01 10:21:07 +0100

    Initial commit

---
+++ src/main/java/pl/example/tmauctionapp/TmAuctionAppApplication.java
@@ -0,0 +1,13 @@
+package pl.example.tmauctionapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmAuctionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmAuctionAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/example/tmauctionapp/TmAuctionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.example.tmauctionapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmAuctionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
