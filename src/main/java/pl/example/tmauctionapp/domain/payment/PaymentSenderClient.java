61a97642a03dfe7d08104d2d58d8406003a2460e
Tomasz Massalski <tomasz.massalski@gmail.com>
2020-03-05 19:43:34 +0100

    bank app communication

--- src/main/java/pl/example/tmauctionapp/domain/payment/PaymentSenderClient.java
+++ src/main/java/pl/example/tmauctionapp/domain/payment/PaymentSenderClient.java
@@ -2,5 +2,8 @@ package pl.example.tmauctionapp.domain.payment;
 import pl.example.tmauctionapp.domain.order.Order;
 
 public interface PaymentSenderClient {
-    Payment sendPayment(Order order);
+    void sendPayment(Order order);
+
+
+
 }
--- src/main/java/pl/example/tmauctionapp/infrastructure/payment/PaymentRestSender.java
+++ src/main/java/pl/example/tmauctionapp/infrastructure/payment/PaymentRestSender.java
@@ -1,18 +1,36 @@
 package pl.example.tmauctionapp.infrastructure.payment;
 
-import lombok.AllArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import pl.example.tmauctionapp.domain.order.Order;
 import pl.example.tmauctionapp.domain.payment.Payment;
 import pl.example.tmauctionapp.domain.payment.PaymentSenderClient;
 
 @Service
-@AllArgsConstructor
 public class PaymentRestSender implements PaymentSenderClient {
 
+    private final RestTemplate restTemplate;
+    private final String transferBankAppUrl;
+
+    @Autowired //TODO sprawdź
+    public PaymentRestSender(RestTemplate restTemplate,
+                             @Value("${send.payment.bankapp.url") String transferBankAppUrl) {
+        this.restTemplate = restTemplate;
+        this.transferBankAppUrl = transferBankAppUrl;
+    }
+
     @Override
-    public Payment sendPayment(Order order) {
-        //TODO complete REST statement
-        return Payment.generate(order);
+    public void sendPayment(Order order) {
+        HttpEntity<Payment> requestEntity = new HttpEntity<>(Payment.generate(order));
+        restTemplate.exchange(transferBankAppUrl,
+                HttpMethod.POST,
+                requestEntity,
+                Void.class);
     }
 }
